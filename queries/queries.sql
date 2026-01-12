e50ccfa307f81d0493ca3146f06b0d11b4cb429f
Riu <josemanuelriu@gmail.com>
2026-01-12 17:22:32 +0100

    Add SQL queries for product listings
    Added SQL queries to list product names and prices.

--- queries/queries.sql
+++ queries/queries.sql
@@ -2,13 +2,13 @@
 SELECT nombre FROM producto;
 
 -- 2. Llista els noms i els preus de tots els productes de la taula producto.
-
+SELECT nombre, precio FROM producto;
 
 -- 3. Llista totes les columnes de la taula producto.
-
+SELECT * FROM producto;
 
 -- 4. Llista el nom dels productes, el preu en euros (precio_eur) i el preu en dòlars estatunidencs (precio_usd) amb un tipus de canvi de 1 € = 1 $.
-
+SELECT nombre, precio as precio_eur, precio as precio_usd  FROM producto;
 
 -- 5. Llista el nom dels productes, el preu en euros i el preu en dòlars estatunidencs (amb un tipus de canvi de 1 € = 1,1 $ i arrodonint el resultat a dues xifres decimals). Utilitza els següents àlies per a les columnes: nom del producte, euros, dòlars.
 
